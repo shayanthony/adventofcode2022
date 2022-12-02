636c73e26ac8aa821c62556ccfffba10e3de19f4
Shane Cable <cableincorp@gmail.com>
2022-12-02 07:43:24 +0000

    feat(*): Question 2 solved

--- adventofcode/src/app/app.component.ts
+++ adventofcode/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { inputArray } from 'src/assets/data';
+import { strategyArray } from 'src/assets/strategy-data';
 import { Elf } from 'src/model/elf';
 
 @Component({
@@ -34,10 +35,88 @@ export class AppComponent {
     elves.sort((a, b) => b.totalCalories - a.totalCalories);
     
     // Q1
-    const bestElf = elves[0];
+    const bestElf = elves[0].totalCalories;
     // Q1.2
     const totalCaloriesOfTopThreeElves = elves[0].totalCalories + elves[1].totalCalories + elves[2].totalCalories
     console.log(totalCaloriesOfTopThreeElves)
   }
+
+  doTwo() {
+    let arrays = strategyArray.trim().split("\n");
+    let totalScore = 0;
+    arrays.forEach(game => {
+      let combinedScore = 0;
+      let valueOne = game.charAt(0);
+      let valueTwo = game.charAt(2)
+      
+      switch (valueTwo)
+      {
+        case "Y": 
+        combinedScore += 2
+        break;
+        case "X":
+        combinedScore += 1
+        break;
+        case "Z":
+        combinedScore += 3
+        break; 
+      }
+
+      combinedScore += this.calculateWinner(valueOne, valueTwo)
+      totalScore += combinedScore;
+      combinedScore = 0;
+    })
+
+    console.log(totalScore)
+  }
+
+  calculateWinner(valueOne: any, valueTwo: any) {
+    // Rock beats scissors
+    // Paper beats rock
+    // Scissors beats paper
+    let score = 0;
+
+    if (valueOne === ElfHand.ROCK)
+    {
+      if (valueTwo === YourHand.ROCK)
+      {
+        score = 3
+      }
+      else if (valueTwo === YourHand.PAPER) {
+        score = 6;
+      }
+    }
+
+    if (valueOne === ElfHand.PAPER) {
+      if (valueTwo === YourHand.PAPER) {
+        score = 3;
+      } else if (valueTwo === YourHand.SCISSORS) {
+        score = 6
+      }
+    }
+
+    if (valueOne === ElfHand.SCISSORS) {
+      if (valueTwo === YourHand.ROCK) {
+        score = 6
+      } else if (valueTwo === YourHand.SCISSORS) {
+        score = 3
+      }
+    }
+
+    console.log(score)
+    return score
+  }
+}
+
+enum ElfHand {
+  ROCK = "A",
+  PAPER = "B",
+  SCISSORS = "C"
+}
+
+enum YourHand {
+  ROCK = "X",
+  PAPER = "Y",
+  SCISSORS = "Z"
 }
 
